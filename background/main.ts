2b7741db5555c419ce53da800fec5602fb67c19c
Matt Luongo <mhluongo@gmail.com>
2021-12-28 22:11:30 -0500

    Key Redux account data by address and chain ID
    While this is a huge improvement over just keying by address, it's not
    quite as general as we'd originally planned. Keying by the more general
    Network type doesn't make sense at this stage, so I'm leaving that for
    when we support non-EVM chains.
    
    Refs #5, refs #7, refs #765

--- background/main.ts
+++ background/main.ts
@@ -30,6 +30,7 @@ import { AddressNetwork } from "./accounts"
 
 import rootReducer from "./redux-slices"
 import {
+  newKeyringAddress,
   loadAccount,
   updateAccountBalance,
   updateENSName,
@@ -513,7 +514,7 @@ export default class Main extends BaseService<never> {
     const existingAccounts = await this.chainService.getAccountsToTrack()
     existingAccounts.forEach((addressNetwork) => {
       // Mark as loading and wire things up.
-      this.store.dispatch(loadAccount(addressNetwork.address))
+      this.store.dispatch(loadAccount(addressNetwork))
 
       // Force a refresh of the account balance to populate the store.
       this.chainService.getLatestBaseAccountBalance(addressNetwork)
@@ -593,7 +594,7 @@ export default class Main extends BaseService<never> {
 
     this.keyringService.emitter.on("address", (address) => {
       // Mark as loading and wire things up.
-      this.store.dispatch(loadAccount(address))
+      this.store.dispatch(newKeyringAddress(address))
 
       this.chainService.addAccountToTrack({
         address,
